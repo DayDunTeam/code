8a41360c60789522a976ac1085748e684faabd00
Carl Sundvall <TheDayDun@gmail.com>
2016-05-11 23:51:49 +0200

    More layout

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ var memory = [
     null, null, null, null
 ];
 
-var code = [
+var program = [
     ["input"],
     ["copyto", 0, false],
     ["input"],
@@ -35,34 +35,46 @@ window.onload = function() {
 };
 
 function stepCode() {
-    if (code[step][0] == "input") {
+    if (program[step][0] == "input") {
         holding = input.shift();
-    } else if (code[step][0] == "output") {
+    } else if (program[step][0] == "output") {
         output.push(holding);
         holding = null;
-    } else if (code[step][0] == "copyto") {
-        memory[code[step][1]] = holding;
-    } else if (code[step][0] == "copyfrom") {
-        holding = memory[code[step][1]];
-    } else if (code[step][0] == "jump") {
-        step = code[step][1] - 1;
+    } else if (program[step][0] == "copyto") {
+        memory[program[step][1]] = holding;
+    } else if (program[step][0] == "copyfrom") {
+        holding = memory[program[step][1]];
+    } else if (program[step][0] == "jump") {
+        step = program[step][1] - 1;
     }
     step++;
     update();
 }
 
 function update() {
+    document.getElementById("programlist").innerHTML = "";
+    for (i=0; i<program.length; i++) {
+        document.getElementById("programlist").innerHTML += "<li><span class=\"command\">" + program[i][0] + "</span></li>";
+    }
+    
+    
     document.getElementById("inputlist").innerHTML = "";
     for (i=0; i<input.length; i++) {
-        document.getElementById("inputlist").innerHTML =+ "<li>" + input[i] + "</li>";
+        document.getElementById("inputlist").innerHTML += "<li><span class=\"number\">" + input[i] + "</span></li>";
     }
-    document.getElementById("holding").innerHTML = "Holding: " + holding;
+    if (holding) {
+        document.getElementById("holding").innerHTML = "Holding: " + "<span class=\"number\">" + holding + "</span>";
+    } else {
+        document.getElementById("holding").innerHTML = "Holding: none";
+    }
     document.getElementById("step").innerHTML = "Step: " + step;
     document.getElementById("outputlist").innerHTML = "";
     for (i=0; i<output.length; i++) {
-        document.getElementById("outputlist").innerHTML =+ "<li>" + output[i] + "</li>";
+        document.getElementById("outputlist").innerHTML += "<li><span class=\"number\">" + output[i] + "</span></li>";
     }
     for (i=0; i<16; i++) {
-        document.getElementById("memory" + i).innerHTML = memory[i];
+        if (memory[i]) {
+            document.getElementById("memory" + i).innerHTML = "<span class=\"number\">" + memory[i] + "</span>";
+        }
     }
 }
